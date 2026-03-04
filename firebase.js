fc8bcad1cb32264f7a8982ec59de66e1adf938f1
Mudassir Shaikh <mudassirakram30@gmail.com>
2026-03-04 23:36:43 +0500

    layout changes

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+import { query, orderBy } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
 import {
   getFirestore,
